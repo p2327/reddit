23ddcf63f54c9530f332b2588747200d7ba0415d
p2327 <desaru@protonmail.com>
2020-03-14 11:43:18 +0000

    completed web app

--- reddit/emailer/__init__.py
+++ reddit/emailer/__init__.py
@@ -5,15 +5,15 @@ from .mailgun_emailer import send_email
 
 
 def email_last_scraped_date():
-    # connect to db
+    # Connect to db
     MONGO_URI = 'mongodb+srv://pete:d71RZ9Xu46NkPxL7@paws-sandbox-gdqa2.mongodb.net/paws-sandbox?ssl=true&ssl_cert_reqs=CERT_NONE'
     connect('paws-sandbox', host=MONGO_URI)
 
-    ## get the last date the webscraper was run
+    # Get the last date the webscraper was run by loading the latest post added
     for post in Post.objects().fields(date_str=1).order_by('-date_str').limit(1):
         day_to_pull = post.date_str
 
-    ## pass in variables, render template, and send
+    # Pass in variables, render template, and send
     context = {
         'day_to_pull': day_to_pull,
         'Post': Post,
